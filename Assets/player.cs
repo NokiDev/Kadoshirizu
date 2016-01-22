ae5d2c21aa661468bd3f1c5c8f881c1615236ac2
Guillaume <guillaumenougier@gmail.com>
2016-01-22 14:50:15 +0100

    test

---
+++ Assets/player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    public int lifePoint;
+    private int nbDots;
+
+    // Use this for initialization
+    void Start () {
+        this.lifePoint = 5000;
+        this.nbDots = 0;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Dots(){
+        nbDots++;
+        if(nbDots == 3){
+            Damage(1000);
+        }
+    }
+
+    void Damage(int damage){
+        this.lifePoint -= damage;
+    }
+
+
+}
